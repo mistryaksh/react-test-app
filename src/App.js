f7f9e75085756d32b2ff9fbe072e746392bae71b
mistryaksh <mistryaksh1998@gmail.com>
2023-01-02 15:31:05 +0530

    adding more functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 
 function App() {
-  const GettingRequest = async () => {
+  const [kioskId, setKioskId] = useState('')
+  const [data, setData] = useState()
+  const [email, setEmail] = useState('')
+  const [pin, setPin] = useState('')
+
+
+  const GettingRequest = async (kioskData) => {
     try {
-      const getRequest = await axios.get(`http://swayamhealth.info/api/category/gettestfromkiosk/MUMSAN2`, {
+      console.log(kioskData.kioskData)
+      const getRequest = await axios.get(`http://swayamhealth.info/api/category/gettestfromkiosk/${kioskData.kioskData}`, {
         headers: {
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTION',
         }
       })
-      console.log("GET DATA", getRequest.data.data.result)
+      console.log(kioskData)
+      setData(getRequest.data.result)
+      console.log("GET DATA", data)
     } catch (err) {
       if (err.response) {
         console.log(err.response.data.message)
@@ -20,11 +29,11 @@ function App() {
       console.log(err.message)
     }
   }
-  const PostingRequest = async () => {
+  const PostingRequest = async (emailData, pinData) => {
     try {
       const postRequest = await axios.post(`http://swayamhealth.info/api/organisation/orglogin`, {
-        email: "mylabs@gmail.com",
-        password: "mylabs"
+        email: emailData,
+        password: pinData
       }, {
         headers: {
           'Access-Control-Allow-Origin': '*',
@@ -39,14 +48,45 @@ function App() {
       console.log(err.message)
     }
   }
+
+  const handleGetData = () => {
+    GettingRequest({ kioskData: kioskId })
+  }
+  const handlePostRequest = () => {
+    PostingRequest(email, pin)
+  }
+
   return (
     <div className="App">
       <h1>Hello world! Click button for calling request</h1>
       <div style={{
-        display: "flex", gap: 5
+        display: "flex", gap: 5,
+        flexDirection: "column"
       }}>
-        <button type="button" onClick={GettingRequest}>Call get request</button>
-        <button type="button" onClick={PostingRequest}>Call post request</button>
+        <div>
+
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            handleGetData()
+          }}>
+            <input type="kioskId" name="kioskId" value={kioskId} onChange={e => setKioskId(e.target.value)} />
+            <button type="submit" >Call get request</button>
+          </form>
+        </div>
+        <br />
+        <br />
+        <div style={{ display: "flex", width: "100%" }}>
+          <form onSubmit={e => {
+            e.preventDefault()
+            handlePostRequest(email, pin)
+          }}>
+            <input placeholder='Email' type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <br />
+            <input placeholder='Your pin' type="password" name="pin" value={pin} onChange={(e) => setPin(e.target.value)} />
+            <br />
+            <button type="submit">Call post request</button>
+          </form>
+        </div>
       </div>
     </div>
   );
