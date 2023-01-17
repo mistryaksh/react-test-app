7189faad61a00b157e7fb00ae1d55fd3eefa3514
Aakash-mistry <111272027+mistryaksh@users.noreply.github.com>
2023-01-17 12:24:10 +0530

    cleared changes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,93 @@
-import React, { useState } from 'react';
-import './App.css';
-import { GettingRequestAxios, PostingRequestAxios } from './utils';
+import React, { useState } from "react";
+import "./App.css";
+import { GettingRequestAxios, PostingRequestAxios } from "./utils";
 
 function App() {
-  const [kioskId, setKioskId] = useState('')
-  const [data, setData] = useState()
-  const [phone, setPhone] = useState('')
-  const [pin, setPin] = useState('')
+     const [kioskId, setKioskId] = useState("");
+     const [data, setData] = useState();
+     const [phone, setPhone] = useState("");
+     const [pin, setPin] = useState("");
 
-  const handlePostRequest = async () => {
-    const YourData = await PostingRequestAxios(phone, pin)
-    setData(YourData)
-    console.log('POST_DATA', data)
-    setPhone('')
-  }
-  const handleGetData = async () => {
+     // Using axios request // ! GET
+     const handleGetData = async () => {
+          try {
+               const yourData = await GettingRequestAxios(kioskId);
+               console.log("GOT THE DATA", yourData);
+          } catch (err) {
+               if (err.response) {
+                    console.log("SERVER ERROR", err.response.data.message);
+               } else {
+                    console.log("NORMAL ERROR", err.message);
+               }
+          }
+     };
 
-    // Using fetch request
+     // Use axios request // ! POST
+     const handlePostRequest = async () => {
+          const YourData = await PostingRequestAxios(phone, pin);
+          setData(YourData);
+          console.log("POST_DATA", data);
+          setPhone("");
+     };
 
-    const yourData = await GettingRequestAxios(kioskId)
-    console.log("GOT THE DATA", yourData)
-    return yourData
-  }
-
-  return (
-    <div className="App">
-      <h1>Hello world! Click button for calling request</h1>
-      <div style={{
-        display: "flex", gap: 5,
-        flexDirection: "column"
-      }}>
-        <div>
-
-          <form onSubmit={(e) => {
-            e.preventDefault()
-            handleGetData()
-          }}>
-            <input placeholder='Kiosk ID' type="kioskId" name="kioskId" value={kioskId} onChange={e => setKioskId(e.target.value)} />
-            <button type="submit" >Call get request</button>
-          </form>
-        </div>
-        <br />
-        <br />
-        <div style={{ display: "flex", width: "100%" }}>
-          <form onSubmit={e => {
-            e.preventDefault()
-            handlePostRequest(phone, pin)
-          }}>
-            <input placeholder='Phone' type="email" name="number" value={phone} onChange={(e) => setPhone(e.target.value)} />
-            <br />
-            <input placeholder='Your pin' type="password" name="pin" value={pin} onChange={(e) => setPin(e.target.value)} />
-            <br />
-            <button type="submit">Call post request</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+     return (
+          <div className="App">
+               <h1>Hello world! Click button for calling request</h1>
+               <div
+                    style={{
+                         display: "flex",
+                         gap: 5,
+                         flexDirection: "column",
+                    }}
+               >
+                    <div>
+                         <form
+                              onSubmit={(e) => {
+                                   e.preventDefault();
+                                   handleGetData();
+                              }}
+                         >
+                              <input
+                                   placeholder="Kiosk ID"
+                                   type="kioskId"
+                                   name="kioskId"
+                                   value={kioskId}
+                                   onChange={(e) => setKioskId(e.target.value)}
+                              />
+                              <button type="submit">Call get request</button>
+                         </form>
+                    </div>
+                    <br />
+                    <br />
+                    <div style={{ display: "flex", width: "100%" }}>
+                         <form
+                              onSubmit={(e) => {
+                                   e.preventDefault();
+                                   handlePostRequest(phone, pin);
+                              }}
+                         >
+                              <input
+                                   placeholder="Phone"
+                                   type="email"
+                                   name="number"
+                                   value={phone}
+                                   onChange={(e) => setPhone(e.target.value)}
+                              />
+                              <br />
+                              <input
+                                   placeholder="Your pin"
+                                   type="password"
+                                   name="pin"
+                                   value={pin}
+                                   onChange={(e) => setPin(e.target.value)}
+                              />
+                              <br />
+                              <button type="submit">Call post request</button>
+                         </form>
+                    </div>
+               </div>
+          </div>
+     );
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,73 +1,60 @@
-import axios from "axios"
+import axios from "axios";
 
-export const HeaderOptions = {
-     'Access-Control-Allow-Origin': 'ORIGIN',
-     'Access-Control-Allow-Credentials': true,
-     'Access-Control-Allow-Methods': 'PUT, GET, HEAD, POST, DELETE, OPTIONS',
-     'Access-Control-Allow-Headers': 'include',
-     'Content-Type': 'application/json',
-     "origin": "https://react-test-app-1.netlify.app/"
-}
+// export const HeaderOptions = {
+//      "Access-Control-Allow-Origin": "*",
+//      "Access-Control-Allow-Credentials": "true",
+//      "Access-Control-Allow-Methods": "PUT, GET, HEAD, POST, DELETE, OPTIONS",
+//      "Access-Control-Allow-Headers": "*",
+//      "Content-Type": "application/json",
+// };
 
-const BackendUrl = `https://swayamhealth.info/api`
+const BackendUrl = `https://swayamhealth.info/api`;
 // const BackendUrl = "http://43.204.35.128:5000"
 
-const header = new Headers();
-header.append('Access-Control-Allow-Origin', '*');
-
+// axios.defaults.baseURL = BackendUrl;
+// axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+// axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 const GettingRequestAxios = async (kioskData) => {
-     console.log(BackendUrl)
-     const postRequest = await axios.get(`${BackendUrl}/category/gettestfromkiosk/DELSAN2`,
-          {
-               headers: header
-          })
-     console.log(postRequest.data)
-     return postRequest.data.data.org
-
-
-}
-
-const PostingRequestAxios = async (emailData, pinData) => {
      try {
-          const postRequest = await axios.post(`${BackendUrl}/organisation/orglogin`, {
-               email: emailData,
-               password: pinData
-          },
-               {
-                    headers: HeaderOptions
-               })
-          console.log(postRequest.data)
-          return postRequest.data.data.org
+          const getRequest = await axios.get(`${BackendUrl}/category/gettestfromkiosk/${kioskData}`);
+          return await getRequest.data;
      } catch (err) {
           if (err.response) {
-               console.log(err.response.data.message)
+               console.log(err.response.data.message);
           }
-          console.log(err.message)
+          console.log(err);
      }
-}
+};
+const PostingRequestAxios = async (emailData, pinData) => {
+     try {
+          const postRequest = await axios.post(
+               `${BackendUrl}/organisation/orglogin`,
+               {
+                    email: emailData,
+                    password: pinData,
+               },
+               {
+                    headers: HeaderOptions,
+               }
+          );
+          return postRequest.data.data.org;
+     } catch (err) {
+          if (err.response) {
+               console.log(err.response.data.message);
+          }
+          console.log(err.message);
+     }
+};
 const GettingRequestFetch = async (kioskData) => {
-     const data = await fetch(`http://swayamhealth.info/api/category/gettestfromkiosk/${kioskData}`, { method: "GET" }).then(res => {
-          console.log(res.json())
-     }).catch(err => {
-          console.log(err)
-     })
-     return data
-}
+     const data = await fetch(`http://swayamhealth.info/api/category/gettestfromkiosk/${kioskData}`, { method: "GET" })
+          .then((res) => {
+               console.log(res.json());
+          })
+          .catch((err) => {
+               console.log(err);
+          });
+     return data;
+};
 
-export {
-     GettingRequestAxios, PostingRequestAxios, GettingRequestFetch
-}
-
-     // const getRequest = await axios.get(`${BackendUrl}/category/gettestfromkiosk/${kioskData}`, {
-     //      withCredentials: false,
-     //      validateStatus: () => true,
-     //      headers: {
-     //           'Access-Control-Allow-Origin': '*',
-     //           'Content-Type': 'application/json',
-     //           withCredentials: true,
-     //           mode: 'no-cors',
-     //      }
-     // })
-     // console.log("YOUR HEADERS", getRequest.headers)
-     // return await getRequest.data
+export { GettingRequestAxios, PostingRequestAxios, GettingRequestFetch };
